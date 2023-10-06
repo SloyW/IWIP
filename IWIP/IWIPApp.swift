d71a4092728e79ed8da5664f63a41e552e36e13a
Silvester Nita <s.nita@nedeco.de>
2023-10-06 19:39:12 +0200

    Initial Commit

---
+++ IWIP/IWIPApp.swift
@@ -0,0 +1,17 @@
+//
+//  IWIPApp.swift
+//  IWIP
+//
+//  Created by Silvester Nita on 06.10.23.
+//
+
+import SwiftUI
+
+@main
+struct IWIPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
